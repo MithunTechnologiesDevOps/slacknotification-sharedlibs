1006e63a645951f0c66e5c30db8fc5e0f8524039
Mithun Technologies <devopstrainingblr@gmail.com>
2021-12-19 07:46:04 +0530

    Updated the comments

--- vars/sendSlackNotiifcation.groovy
+++ vars/sendSlackNotiifcation.groovy
@@ -1,5 +1,7 @@
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
+  //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
+ 
   buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
   // Default values
